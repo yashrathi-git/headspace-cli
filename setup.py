24c12769c3d8958d9c75283b43f7a1ea5426db58
Yash Rathi <yashrathicricket@gmail.com>
2021-03-13 08:01:44 +0530

    Updated version, added __init__.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/yashrathi-git/headspace-dl"
 EMAIL = "yashrathicricket@gmail.com"
 AUTHOR = "Yash Rathi"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["requests", "Click", "rich"]
